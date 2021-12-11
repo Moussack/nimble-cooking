6b7a7aec48716d37459bb06b62a0a5ae083df6b8
Moussack <reywahyono@gmail.com>
2021-12-11 11:21:07 +0700

    completed lesson 84

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,19 +1,19 @@
 import { Link } from 'react-router-dom';
+
+// styles
 import './RecipeList.css';
 
 export default function RecipeList({ recipes }) {
    return (
       <div className="recipe-list">
-         {recipes.map((recipe) => {
-            return (
-               <div className="card" key={recipe.id}>
-                  <h2>{recipe.title}</h2>
-                  <p>{recipe.cookingTime} To make</p>
-                  <div>{recipe.method.substring(0, 100)}...</div>
-                  <Link to={`/recipe/${recipe.id}`}>Cook This</Link>
-               </div>
-            );
-         })}
+         {recipes.map((recipe) => (
+            <div key={recipe.id} className="card">
+               <h3>{recipe.title}</h3>
+               <p>{recipe.cookingTime} to make.</p>
+               <div>{recipe.method.substring(0, 100)}...</div>
+               <Link to={`/recipe/${recipe.id}`}>Cook This</Link>
+            </div>
+         ))}
       </div>
    );
 }
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,5 +1,28 @@
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../../hooks/useFetch';
+
 import './Recipe.css';
 
 export default function Recipe() {
-   return <div>Recipe</div>;
+   const { id } = useParams();
+   const url = `http://localhost:3000/recipes/${id}`;
+   const { data, isPending, error } = useFetch(url);
+
+   return (
+      <div>
+         {error && <p className="error">{error}</p>}
+         {isPending && <p className="loading">Loading...</p>}
+         {data && (
+            <div>
+               <h2>{data.title}</h2>
+               <p>Method : {data.method}</p>
+               <p>Cooking Time : {data.cookingTime}</p>
+               Ingredients :{' '}
+               {data.ingredients.map((ingg) => (
+                  <div>{ingg}</div>
+               ))}
+            </div>
+         )}
+      </div>
+   );
 }

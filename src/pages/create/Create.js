f7f93e296f4dc4c6f631de5351401ff9c02ea908
Moussack <reywahyono@gmail.com>
2021-12-16 14:35:13 +0700

    lesson 87 done

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,75 @@
+import { useRef, useState } from 'react';
+
 // styles
 import './Create.css';
 
 export default function Create() {
-   return <div>Create</div>;
+   const [title, setTitle] = useState('');
+   const [method, setMethod] = useState('');
+   const [cookingTime, setCookingTime] = useState('');
+   const [newIngredient, setNewIngredient] = useState('');
+   const [ingredients, setIngredients] = useState([]);
+   const ingredientInput = useRef(null);
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(title, method, cookingTime, ingredients);
+   };
+
+   const handleAdd = (e) => {
+      e.preventDefault();
+      const ing = newIngredient.trim();
+
+      if (ing && !ingredients.includes(ing)) {
+         setIngredients((prev) => [...prev, ing]);
+      }
+
+      setNewIngredient('');
+      ingredientInput.current.focus();
+   };
+
+   return (
+      <div className="create">
+         <h2 className="page-title">Add a New Recipe</h2>
+         <form onSubmit={handleSubmit}>
+            <label>
+               <span>Recipe title:</span>
+               <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} required />
+            </label>
+
+            <label>
+               <span>Recipe Ingredients</span>
+               <div className="ingredients">
+                  <input
+                     type="text"
+                     ref={ingredientInput}
+                     onChange={(e) => setNewIngredient(e.target.value)}
+                     value={newIngredient}
+                  />
+                  <button className="btn" onClick={handleAdd}>
+                     add
+                  </button>
+               </div>
+            </label>
+            <p>
+               Current Ingredients :{' '}
+               {ingredients.map((ingg) => (
+                  <em key={ingg}>{`${ingg}, `}</em>
+               ))}
+            </p>
+
+            <label>
+               <span>Recipe method:</span>
+               <textarea onChange={(e) => setMethod(e.target.value)} value={method} required />
+            </label>
+
+            <label>
+               <span>Cooking time (minutes)</span>
+               <input type="number" onChange={(e) => setCookingTime(e.target.value)} value={cookingTime} required />
+            </label>
+
+            <button className="btn">Submit</button>
+         </form>
+      </div>
+   );
 }
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -2,26 +2,26 @@ import { useParams } from 'react-router-dom';
 import { useFetch } from '../../hooks/useFetch';
 
 import './Recipe.css';
-
 export default function Recipe() {
    const { id } = useParams();
    const url = `http://localhost:3000/recipes/${id}`;
    const { data, isPending, error } = useFetch(url);
 
    return (
-      <div>
+      <div className="recipe">
          {error && <p className="error">{error}</p>}
          {isPending && <p className="loading">Loading...</p>}
          {data && (
-            <div>
-               <h2>{data.title}</h2>
-               <p>Method : {data.method}</p>
-               <p>Cooking Time : {data.cookingTime}</p>
-               Ingredients :{' '}
-               {data.ingredients.map((ingg) => (
-                  <div>{ingg}</div>
-               ))}
-            </div>
+            <>
+               <h2 className="page-title">{data.title}</h2>
+               <p>Takes {data.cookingTime} to cook</p>
+               <ul>
+                  {data.ingredients.map((ing) => (
+                     <li key={ing}>{ing}</li>
+                  ))}
+               </ul>
+               <p className="method">{data.method}</p>
+            </>
          )}
       </div>
    );

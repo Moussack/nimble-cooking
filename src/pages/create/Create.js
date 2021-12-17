2f094a243d4c98024f7f66389460eb3464d6ee55
Moussack <reywahyono@gmail.com>
2021-12-17 15:28:52 +0700

    lesson 88 done

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,5 @@
 import { useRef, useState } from 'react';
+import { useFetch } from '../../hooks/useFetch';
 
 // styles
 import './Create.css';
@@ -10,10 +11,12 @@ export default function Create() {
    const [newIngredient, setNewIngredient] = useState('');
    const [ingredients, setIngredients] = useState([]);
    const ingredientInput = useRef(null);
+   const { data, isPending, error, postData } = useFetch('http://localhost:3000/recipes', 'POST');
 
    const handleSubmit = (e) => {
       e.preventDefault();
-      console.log(title, method, cookingTime, ingredients);
+      postData({ title, ingredients, method, cookingTime: `${cookingTime} minutes` });
+      //console.log(title, method, cookingTime, ingredients);
    };
 
    const handleAdd = (e) => {
